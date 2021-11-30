06ab0cdd5b21e7c38afbdee733bbf85ed0506bd6
Fabio <fabio.sunna@gmail.com>
2021-11-30 17:51:44 +0100

    Aggiunte le query per la tabella Corsisti
    Aggiunte le query SELECT_CORSISTI, SELECT_CORSISTA_BY_ID, DELETE_CORSISTA

--- GestioneCorsi/src/main/java/com/gestionecorsi/torino/dao/DAOCostants.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/torino/dao/DAOCostants.java
@@ -17,6 +17,9 @@ public interface DAOCostants {
 	
 	//END
 	//CORSISTI
+	public static final String SELECT_CORSISTI = "select cod_corsista, nome_corsista, cognome_corsista from corsista";
+	public static final String SELECT_CORSISTA_BY_ID = "select * from corsista where cod_corsista = ?";
+	public static final String DELETE_CORSISTA = "delete from corsista where cod_corsista = ?";
 	//END
 	//CORSOCORSISTI
 	//END
