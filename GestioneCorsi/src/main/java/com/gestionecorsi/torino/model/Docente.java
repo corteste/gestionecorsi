91ab6ef8c08c72eaf3cdeddc8f4bbf9cf13db371
Stefano Cortese <corteste1996@gmail.com>
2021-11-30 17:34:02 +0100

    Gestione Corsi v0.5.1
    Implemented Docente model

--- GestioneCorsi/src/main/java/com/gestionecorsi/torino/model/Docente.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/torino/model/Docente.java
@@ -1,5 +1,11 @@
 package com.gestionecorsi.torino.model;
 
 public class Docente {
+	
+	private long codDocente;
+	private String nomeDocente;
+	private String cognomeDocente;
+	private String cvDocente;
+	
 
 }
