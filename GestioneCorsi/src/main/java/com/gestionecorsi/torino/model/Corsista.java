3a6b424ff46d4a6a33c6293b482bdeadda7cc5ec
Luca <lucapianta22@gmail.com>
2021-12-01 11:07:53 +0100

    update Corsista.java; CorsoCorsista.java
    Model Corsista e CorsoCorsista

--- GestioneCorsi/src/main/java/com/gestionecorsi/torino/model/Corsista.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/torino/model/Corsista.java
@@ -1,6 +1,43 @@
 package com.gestionecorsi.torino.model;
 
 public class Corsista {
-	//test luca
+	private int codCorsista;
+	private String nomeCorsista;
+	private String cognomeCorsista;
+	private char precedentiFormativi;
+	
+	public int getCodCorsista() {
+		return codCorsista;
+	}
+	public void setCodCorsista(int codCorsista) {
+		this.codCorsista = codCorsista;
+	}
+	public String getNomeCorsista() {
+		return nomeCorsista;
+	}
+	public void setNomeCorsista(String nomeCorsista) {
+		this.nomeCorsista = nomeCorsista;
+	}
+	public String getCognomeCorsista() {
+		return cognomeCorsista;
+	}
+	public void setCognomeCorsista(String cognomeCorsista) {
+		this.cognomeCorsista = cognomeCorsista;
+	}
+	public char getPrecedentiFormativi() {
+		return precedentiFormativi;
+	}
+	public void setPrecedentiFormativi(char precedentiFormativi) {
+		this.precedentiFormativi = precedentiFormativi;
+	}
+	
+	@Override
+	public String toString() {
+		return "Corsista [codCorsista=" + codCorsista + ", nomeCorsista=" + nomeCorsista + ", cognomeCorsista="
+				+ cognomeCorsista + ", precedentiFormativi=" + precedentiFormativi + "]";
+	}
+	
+	
+	
 
 }
