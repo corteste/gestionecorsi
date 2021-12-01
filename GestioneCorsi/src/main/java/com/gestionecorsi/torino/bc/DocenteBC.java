d763d4b06ee667090626d539d6f071464e8bcb14
place97 <54411433+place97@users.noreply.github.com>
2021-12-01 16:02:59 +0100

    add DocenteBC
    add DocenteBC

--- GestioneCorsi/src/main/java/com/gestionecorsi/torino/bc/DocenteBC.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/torino/bc/DocenteBC.java
@@ -4,28 +4,38 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.ayoub.architecture.dao.ArticoloDAO;
-import com.ayoub.architecture.dao.DAOException;
 import com.gestionecorsi.torino.dao.DocenteDAO;
 import com.gestionecorsi.torino.dbaccess.DBAccess;
 import com.gestionecorsi.torino.model.Docente;
 
+/**
+ * Marco Placentino
+ */
 public class DocenteBC {
 	private Connection conn;
 	
 	public DocenteBC() throws ClassNotFoundException, FileNotFoundException, IOException, SQLException {
 		conn = DBAccess.getConnection();
+		
 	}
 	
-	public Docente getByString(String codDocente) {
+	public Docente getByString(String codDocente) throws SQLException {
 		Docente docente = null;
-		try {
-			docente = DocenteDAO.getFactory().getModelByString(conn, codDocente);
-		} catch (SQLException sql) {
-			throw new SQLException();
-		}
-		
+		docente = DocenteDAO.getFactory().getModelByString(conn, codDocente);
 		return docente;
 	}
+	
+	public List<Docente> getAll() throws SQLException{
+		List<Docente> lista_docente=new ArrayList<>();
+		try {
+			lista_docente=DocenteDAO.getFactory().getAll(conn);
+		}catch(SQLException sql) {
+			throw new SQLException();
+		}
+		return lista_docente;
+	}
+
 }
