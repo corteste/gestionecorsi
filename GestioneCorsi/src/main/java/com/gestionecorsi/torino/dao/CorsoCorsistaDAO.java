bad8f549946778856e265e09252876b5e1cde4b5
Luca <lucapianta22@gmail.com>
2021-12-01 12:09:04 +0100

    update CorsoCorsista.java
    Creazione form Create del corsocorsista

--- GestioneCorsi/src/main/java/com/gestionecorsi/torino/dao/CorsoCorsistaDAO.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/torino/dao/CorsoCorsistaDAO.java
@@ -1,7 +1,44 @@
 package com.gestionecorsi.torino.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.gestionecorsi.torino.dao.adapter.CorsoCorsistaDAOAdapter;
+import com.gestionecorsi.torino.model.CorsoCorsista;
 
-public class CorsoCorsistaDAO extends CorsoCorsistaDAOAdapter {
+/**
+ * @author Luca Pianta
+ *
+ */
+public class CorsoCorsistaDAO extends CorsoCorsistaDAOAdapter implements DAOCostants {
+	private ResultSet rs;
+	private Connection conn;
+	private Statement stmt;
+	
+	private CorsoCorsistaDAO() {
+	}
+	
+	
+	public CorsoCorsistaDAO getFactory(){
+		return new CorsoCorsistaDAO();
+	}
 
+	@Override
+	public void createFromModel(Connection conn, CorsoCorsista model) {
+		try {
+			conn.createStatement();
+			stmt.execute(SELECT__CORSO_CORSISTA);
+			rs.moveToInsertRow();
+			rs.updateLong(1, model.getCodCorsista());
+			rs.updateLong(2, model.getCodCorso());
+			rs.insertRow();
+			rs.moveToCurrentRow();
+			conn.commit();	
+		}catch (SQLException sql) {
+			sql.getMessage();
+		}	
+		
+	}
 }
