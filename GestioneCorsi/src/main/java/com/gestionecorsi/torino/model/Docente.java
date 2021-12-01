f115b1d66c72882589901da54c6bef2c5d58e697
Stefano Cortese <corteste1996@gmail.com>
2021-12-01 10:55:43 +0100

    Gestione Corsi v0.5
    Updated Docente model

--- GestioneCorsi/src/main/java/com/gestionecorsi/torino/model/Docente.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/torino/model/Docente.java
@@ -7,6 +7,30 @@ public class Docente {
 	private String cognomeDocente;
 	private String cvDocente;
 	
-
-//SONO SALVO
+	public long getCodDocente() {
+		return codDocente;
+	}
+	public void setCodDocente(long codDocente) {
+		this.codDocente = codDocente;
+	}
+	public String getNomeDocente() {
+		return nomeDocente;
+	}
+	public void setNomeDocente(String nomeDocente) {
+		this.nomeDocente = nomeDocente;
+	}
+	public String getCognomeDocente() {
+		return cognomeDocente;
+	}
+	public void setCognomeDocente(String cognomeDocente) {
+		this.cognomeDocente = cognomeDocente;
+	}
+	public String getCvDocente() {
+		return cvDocente;
+	}
+	public void setCvDocente(String cvDocente) {
+		this.cvDocente = cvDocente;
+	}
+	
+	
 }
